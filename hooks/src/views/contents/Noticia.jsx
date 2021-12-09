218cd48ce840f3c183c109e1c9d1fa8427cf5436
EDUAPWPAM <eduardo.bento01@etec.sp.gov.br>
2021-12-09 22:59:42 +0000

    save9

--- hooks/src/views/contents/Noticia.jsx
+++ hooks/src/views/contents/Noticia.jsx
@@ -5,7 +5,13 @@ const Noticia = props => (
 
     <div>
             <h1>Notícias</h1>
-            
+            <br></br>
+            <br></br>
+            <form action="http://google.com">
+                <label>
+                <input type="submit" value="Go to Google" />
+                </label>
+            </form>
         </div>
 )
 
