32c30e1bc5cac800c7636aaaad3e5116b3403d19
EDUAPWPAM <eduardo.bento01@etec.sp.gov.br>
2021-12-09 22:08:57 +0000

    save6.5

--- hooks/src/views/contents/Comunidade.jsx
+++ hooks/src/views/contents/Comunidade.jsx
@@ -1,10 +1,33 @@
 import React from "react"
 
+
 const Comunidade = props => (
     <div>
+        
         <h1>Comunidade</h1>
         <p>Aqui serão realizados aberturas de pedidos para o site ou duvidas da matéria</p>
+        <br></br>
+        <br>
+        </br>
         
+    <form>
+    <label>
+        E-mail:
+        <input type="email" required="required" name="email" 
+        id="text2" size="50" placeholder="Digite seu email aqui" 
+        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" autofocus=""></input>
+    </label>  
+    </form>
+    <br></br>
+    <br></br>
+
+    <form>
+    <label>
+        <textarea id="textarea" name="textarea " 
+        required="required" rows="15" cols="60" placeholder="Deixe aqui sua mensagem aqui." autofocus=""></textarea>
+    </label>
+    </form>
+
     </div>
 )
 
