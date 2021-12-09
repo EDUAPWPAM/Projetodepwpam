df28e858528195dc353261a6e0e75dc4221eb120
EDUAPWPAM <eduardo.bento01@etec.sp.gov.br>
2021-12-09 21:30:36 +0000

    save 4

--- hooks/src/views/contents/Sobre.jsx
+++ hooks/src/views/contents/Sobre.jsx
@@ -4,9 +4,15 @@ const Sobre = props => (
     <div>
         <h1>Sobre a página</h1>
 
-        <p>Nós somos a rede de notícia do curso <span>EXPECÍFICO</span> do curso de 
-        Ds da escola Etec Hortolândia, aqui informaremos você, aluno(a), sobre possiveis
-        novos amistosos, atualizações sore salas e professores e futuros eventos destinados ao curso.</p>
+        <p>Nós somos a rede de notícias <span>EXPECÍFICAS</span> do curso de 
+        Ds (antigo MI) da escola Etec Hortolândia, aqui informaremos você, aluno(a), sobre possiveis
+        novos amistosos, atualizações sobre salas e professores e futuros eventos destinados ao curso!</p>
+        <br></br>
+        <br></br>
+        <hr></hr>
+        <p>Para poder saber mais sobre a escola e sobre nossa "fa <span>MI</span> lia" acesse:</p>
+        <p><a href="https://www.etechortolandia.com.br/">Etec Hortolândia</a></p>  
+        <p><a href="https://www.vestibulinhoetec.com.br/unidades-cursos/escola.asp?c=31">Vestibulinho Hortolândia</a></p>
     </div>
 )
 
