ae8a027981c4293e48cb4385306911201ad69fd4
EDUAPWPAM <eduardo.bento01@etec.sp.gov.br>
2021-12-10 02:59:13 +0000

    save17

--- hooks/src/views/contents/Home.jsx
+++ hooks/src/views/contents/Home.jsx
@@ -4,7 +4,9 @@ import React from "react"
 const Home = props => (
     <div>
         <h1>Inicial</h1>
-
+        <h2>Sejam Bem-Vindos Fa<span>Mi</span>lia</h2>
+        <br></br>
+        <p><img src="logo-etec.png"/></p>
     </div>
 )
 
